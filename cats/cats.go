00000080c44e8f96ab6969589cba24edd5ae2f6b
Gustav Westling <gustav@getsturdy.com>
2022-11-16 14:59:49 +0100

    cats: add more designs
    magic: MTQAucOo0wEA

--- cats/cats.go
+++ cats/cats.go
@@ -2,14 +2,26 @@ package cats
 
 import _ "embed"
 
-//go:embed cat_1.txt
-var CatDefault string
+//go:embed cat_normal_straight.txt
+var CatNormalStraight string
 
-//go:embed cat_2.txt
-var CatDefaultLookRight string
+//go:embed cat_normal_straight_raised_tail.txt
+var CatNormalStraightRaisedTail string
 
-//go:embed cat_3.txt
-var Cat3 string
+//go:embed cat_normal_straight_folded_left_ear.txt
+var CatNormalStraightFoldedLeftEar string
 
-//go:embed cat_4.txt
+//go:embed cat_normal_straight_folded_right_ear.txt
+var CatNormalStraightFoldedRightEar string
+
+//go:embed cat_normal_left.txt
+var CatNormalLeft string
+
+//go:embed cat_normal_right.txt
+var CatNormalRight string
+
+//go:embed cat_amused.txt
 var CatAmused string
+
+//go:embed cat_curious.txt
+var CatCurious string

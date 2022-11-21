00000250c80e6bf78bbc259265069848199982bb
Gustav Westling <gustav@getsturdy.com>
2022-11-21 11:17:16 +0100

    history: reset to parent
    magic: MTQHw4m1AQAA

--- history.sh
+++ history.sh
@@ -3,21 +3,19 @@
 # This script makes sure that all commit hashes have nice and incremental prefixes.
 # Built with https://github.com/Mattias-/githashcrash
 
-git reset --hard main
-git checkout main
+#git reset --hard main
+#git checkout main
 
-# root_commit=$(git log --format=format:%H --reverse | head -n1)
+# All commits in our repository
 commits=$(git log --format=format:%H --reverse)
 
 git branch -D newmain
 git checkout -b newmain
 
-# git reset --hard "$root_commit"
-
 i=0
 
 # Find start commit
-root_commit=""
+prev_commit=""
 did_reset=0
 
 for sha1 in $commits; do
@@ -26,20 +24,27 @@ for sha1 in $commits; do
 
     ((i=i+1))
 
-    if [[ "$sha1" == $prefix* ]]; then
+
+    if [[ "$sha1" == $prefix* ]] && ((!did_reset)); then
         echo "$sha1 starts with $prefix, doing nothing"
+        prev_commit="$sha1"
         continue
     else
         if ((!did_reset)); then
-            echo "Found first misaligned commit: $sha1"
-            git reset --hard "$root_commit"
-            did_reset=1
-        else
-            git show -s --format=%B "$sha1" > .msg
+            echo "Found first misaligned commit=$sha1 parent=$prev_commit"
+            git reset --hard "$prev_commit"
+            did_reset=1   
+        fi
+
+        git cherry-pick "$sha1"
+
+        git show -s --format=%B "$sha1" > .msg
         echo >> .msg
         echo "magic: REPLACEME" >> .msg
         cat .msg | git commit --amend -F -
         ~/src/githashcrash/githashcrash "$prefix" | bash
-        fi
     fi
 done
+
+git branch -D main
+git checkout -b main
